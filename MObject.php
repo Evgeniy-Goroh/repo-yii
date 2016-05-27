4c2947c39dbeb543ac201522fdd5f5e9437ab611
Evgeniy <evgeniy.goroh@gmail.com>
2016-05-27 23:36:25 +0400

    add construct

--- MObject.php
+++ MObject.php
@@ -5,6 +5,21 @@
 class MObject{
 	protected $_data = array();
 	
+	public function __call($method,$args){
+		$action = substr($method,0,3);
+		$key = substr($method,3);
+		switch ($action){
+			case 'get':
+				return $this->getData($key);
+			case 'set':
+				return $this->satdata($key, $args[0]);
+		}
+	}
+	
+	public function __construct($initialData){
+		$this->_data = $initialData;
+	}
+	
 	public function setData($key,$value=null){
 		if(is_array($key)){
 			this->_data = $key; 
